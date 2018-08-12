f317e538d36c5ce6453ca968cd069f593d2fab4f
rubixibuc <rubixibuc@gmail.com>
2018-08-12 16:53:42 -0400

    update todos

--- reactor/src/index.js
+++ reactor/src/index.js
@@ -46,8 +46,9 @@ ReactDOM.render(
 // TODO: convert render when to recompose branch hoc -> done
 // TODO: consolidate reactor core webpack configs -> done
 // TODO: try referencing redux from core reactor core -> done
-// TODO: create meta orchestrator/bootstrap core
-// TODO: test redux
+// TODO: create meta orchestrator/bootstrap core -> done
 // TODO: install react router
+// TODO: test redux
 // TODO: load 1 route dynamically
 // TODO: load multiple routes dynamically
+// TODO: include all dependencies in package.json inside vendors dll automatically
