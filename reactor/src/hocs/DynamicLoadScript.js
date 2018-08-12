bea486b5cb2ec1b92664e8d983e19746962d99a3
rubixibuc <rubixibuc@gmail.com>
2018-08-12 10:58:14 -0400

    prettier

--- reactor/src/hocs/DynamicLoadScript.js
+++ reactor/src/hocs/DynamicLoadScript.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 import scriptLoader from "react-async-script-loader";
 
-export const DynamicLoadScript = ({scripts, children}) => {
-    const Component = scriptLoader(scripts)(() => <React.Fragment>{children}</React.Fragment>);
-    return <Component/>
+export const DynamicLoadScript = ({ scripts, children }) => {
+  const Component = scriptLoader(scripts)(() => (
+    <React.Fragment>{children}</React.Fragment>
+  ));
+  return <Component />;
 };
